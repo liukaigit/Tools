ebd3b5cba3e581f67737392b78fb4e8f7f2a5ce3
41host <liukai_mr@163.com>
2016-04-14 19:47:09 +0800

    add print_error

--- src/lkfunc.c
+++ src/lkfunc.c
@@ -1,23 +1,29 @@
 #include<stdio.h>
-//#include<unistd.h>
-//#include<sys/types.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
 
 #define MAX_LEN 1024
 
-/*Get the specified field in the string*/
+/*Print the error information.*/
+void print_error(const char *error){
+    printf("-_-! \033[1;31mError:\033[0m\n");
+}
+
+/*Get the specified field in the string.*/
 int GetFieldFromLine(const char *src,char *des,int nth){
 	int cnt = 1;
 	const char *tmpsrc = src;
 	char *tmpdes = des;
 	if(nth < 1){
-		//printf("Error:the number of specified field is too small\n");
+		print_error("the number of specified field is too small\n");
 		return -1;
 	}
-	/*过滤空格*/
+	/*Filter the blank space.*/
 	while(*tmpsrc == ' '){
 		tmpsrc++;
 	}
-	/*过滤空行*/
+	/*Filter the blank line.*/
 	if(*tmpsrc == '\r' || *tmpsrc == '\n' || *tmpsrc == '\0'){
 		return -1;
 	}
@@ -29,7 +35,7 @@ int GetFieldFromLine(const char *src,char *des,int nth){
 		}
 		/*检测行是否结束，来判断是否有指定的字段存在*/
 		if(*tmpsrc == '\r' || *tmpsrc == '\n' || *tmpsrc == '\0'){
-			//printf("Error:the number of specified field is too big\n");
+			print_error("the number of specified field is too big\n");
 			return -1;
 		}
 		/*过滤空格*/
